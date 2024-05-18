a9441ecb509b0f972b229230ccfeb7afce1b1c41
Nick Rabone <n.rabone72@aol.com>
2024-05-18 04:52:06 +0100

    Continued Register Page.

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -12,6 +12,19 @@ const Register = () => {
                         <label htmlFor="name">Name</label>
                         <input className='px-3 py-2 outline-none text-[#111] bg-[#eee] rounded-md' type="text" name='name' placeholder='Name' id='name' required/>
                     </div>
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="email">Email</label>
+                        <input className='px-3 py-2 outline-none text-[#111] bg-[#eee] rounded-md' type="text" name='email' placeholder='Email' id='email' required/>
+                    </div>
+                    <div className='flex flex-col w-full gap-1 mb-3'>
+                        <label htmlFor="password">Password</label>
+                        <input className='px-3 py-2 outline-none text-[#111] bg-[#eee] rounded-md' type="password" name='password' placeholder='Password' id='name' required/>
+                    </div>
+                    <div className='flex item-center w-full gap-3 mb-3'>
+                        <input className='w-4 h-4 text-[#0000ff]overflow-hidden bg-[#ccc] rounded border-[#ccc] focus:ring-[#0000ff]' type='checkbox' name='checkbox' id='checkbox' />
+                        <label htmlFor="checkbox">I agree to terms and conditions</label>
+                    </div>
+                    <button className='bg-[#eee] text-[#111] w-full rounded-md px-7 py-2 hover:bg-[#ccc]'>Sign Up</button>
                 </form>
             </div>
         </div>
