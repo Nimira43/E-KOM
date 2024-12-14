8eb16b5a6ea96f2065c2509a884a3ad48a50f883
Nimira43 <n.rabone72@aol.com>
2024-12-14 05:06:22 +0000

    Realigning text.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class PDF(FPDF):
     self.image('billy.png', 10, 8, 22)
     # (<image>,<x co-ord>, <y co-ord>, <width>)
     self.set_font('helvetica', 'B', 16)
-    self.cell(40, 10, 'Testing')
+    self.cell(80)
+    self.cell(40, 10, 'NimiraTech')
 
 pdf = PDF()
 
