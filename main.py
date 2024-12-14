8e2b2fa7eefb33d58f4ee5f2fa866519aeb66d28
Nimira43 <n.rabone72@aol.com>
2024-12-14 05:01:21 +0000

    Small refactor

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ class PDF(FPDF):
   def header(self):
     self.image('billy.png', 10, 8, 22)
     # (<image>,<x co-ord>, <y co-ord>, <width>)
+    self.set_font('helvetica', 'B', 16)
+    self.cell(40, 10, 'Testing')
 
 pdf = PDF()
 
 pdf.add_page()
-pdf.set_font('helvetica', 'B', 16)
-pdf.cell(40, 10, 'Testing')
 pdf.output('test2.pdf')
